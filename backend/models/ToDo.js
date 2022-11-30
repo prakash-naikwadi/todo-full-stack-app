e45acf094b74579a01ddf58322c14cafd16700bd
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-11-30 13:20:17 +0530

    Schema updated, added min and max length for title and description

--- backend/models/ToDo.js
+++ backend/models/ToDo.js
@@ -3,8 +3,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
-  title: String,
-  tasks: [{ description: String }],
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 25,
+  },
+  tasks: [
+    {
+      description: {
+        type: String,
+        minlength: 1,
+        maxlength: 100,
+      },
+    },
+  ],
 });
 
 const TodoModel = mongoose.model("Todo", TodoSchema);
