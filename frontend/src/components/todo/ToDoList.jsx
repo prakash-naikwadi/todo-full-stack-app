ef91daa7ece21660e7974209b987a43c245028d9
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-11-30 19:42:23 +0530

    ascending and descending sorting functionality addded using API, radio btns added for same

--- frontend/src/components/todo/ToDoList.jsx
+++ frontend/src/components/todo/ToDoList.jsx
@@ -6,14 +6,32 @@ import { useRef } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+import axios from "axios";
 
 const ToDoList = ({ todos, setTodos, fetchTodosData }) => {
   const [showToDoForm, setShowToDoForm] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("des");
   let elementReference = useRef();
 
   const handleAdd = () => {
     setShowToDoForm((prev) => !prev);
   };
+
+  const handleRadioChange = (e) => {
+    console.log(e.target.checked);
+    console.log(e.target.value);
+    setSelectedOption(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchData = async (sort) => {
+      const res = await axios.get(`/getToDos/sort/${sort}`);
+      console.log(res.data.sortedData);
+      setTodos(res.data.sortedData);
+    };
+    fetchData(selectedOption);
+  }, [selectedOption, setTodos]);
 
   return (
     <>
@@ -29,6 +47,32 @@ const ToDoList = ({ todos, setTodos, fetchTodosData }) => {
                 />
               </button>
             )}
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <div className="mr-2">
+            <label htmlFor="flexRadioDefault10">Asc</label>
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault10"
+              value="asc"
+              checked={selectedOption === "asc"}
+              onChange={handleRadioChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="flexRadioDefault20">Des</label>
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault20"
+              value="des"
+              checked={selectedOption === "des"}
+              onChange={handleRadioChange}
+            />
           </div>
         </div>
         <ul className="mt-4">
