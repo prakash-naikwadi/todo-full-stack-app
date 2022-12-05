91e9830525c9321482cb5b5ab50a8903de94ba18
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-12-05 11:46:52 +0530

    or port changed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
