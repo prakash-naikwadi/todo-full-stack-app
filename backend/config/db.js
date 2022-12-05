cfdd7759e2c5799e643dbd4c0f57693b3dfce64b
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-12-05 11:13:29 +0530

    database configuration added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectToDB = () => {
   mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then((conn) => {
       console.log(`Connected To DB ${conn.connection.host}`);
     })
